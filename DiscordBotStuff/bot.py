c2f3bea5ead4a821efc0e6947b5372105d4da03b
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-24 16:36:40 -0400

    Uploading test code in bot.py for testing on Windows machine.

--- DiscordBotStuff/bot.py
+++ DiscordBotStuff/bot.py
@@ -21,23 +21,14 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+    await ctx.send(f"Kinda useless stuff, but ok... {round(client.latency * 1000)} ms")
 
-
-# draw_schedule(TermSchedule(list(map(int, crn_codes.split()))))
 
 @client.command(aliases=["new", "generate"])
 async def make_schedule(ctx, *, course_inputs):
     all_courses_list = get_clean_courses_list(course_inputs)
-    main_schedules_list = generate(all_courses_list)[0]
-    ctx.send(f"A: {main_schedules_list}")
+    max_schedules = generate(all_courses_list)
+    ctx.send(f"First: {max_schedules[0]}")
 
-
-"""
-async def make_image(ctx, *, crn_codes):
-with open("..//PNGMaker/schedule.png", "rb") as f:
-f = discord.File(f, filename="schedule.png")
-await ctx.send(file=f)
-"""
 
 client.run(CLIENT_TOKEN)

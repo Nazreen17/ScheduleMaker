555794298620473bc7967115f6fae8e853491c8f
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-24 12:35:14 -0400

    Added find_from_crn() for usage soon in flip_clock_combinations(). Cleaned up a bit of generate().

--- JSONMaxTemplates/MaxTemplateGeneration.py
+++ JSONMaxTemplates/MaxTemplateGeneration.py
@@ -15,12 +15,15 @@ def generate(course_obj_list):
     # Second dimension element = single course list of all its options
     # Third dimension element = a single option of the parent course
 
-    for course_obj in course_obj_list:
-        all_course_classes = pull_class(fac=course_obj.fac, uid=course_obj.uid, seats=1)
+    all_course_classes = []
 
+    for course_obj in course_obj_list:
+        all_course_classes.append(pull_class(fac=course_obj.fac, uid=course_obj.uid, seats=1))  # Pull all classes
+
+    for course_sublist in all_course_classes:  # Cycle through each course sublist to compute in 2d list format
         course_2d_sublist = []
 
-        for class_obj in all_course_classes:
+        for class_obj in course_sublist:  # Cycle all class objects
             if class_obj.class_type == "Lecture":
 
                 if len(class_obj.links) > 0:  # The lecture class has at least 1 linked secondary class option
@@ -39,7 +42,7 @@ def generate(course_obj_list):
     # ... etc
     # ]
 
-    main_schedules_list = __flip_clock_combinations(list_3d)
+    main_schedules_list = __flip_clock_combinations(list_3d, all_course_classes)
     print(f"\tGenerated {len(main_schedules_list)} CRN simplified schedules ({str(round(time.time() - start, 2))} sec)")
 
     main_schedules_list = __crn_clean_up(main_schedules_list)  # converting MaxSchedule to a clear list of CRN codes
@@ -47,7 +50,7 @@ def generate(course_obj_list):
     return main_schedules_list
 
 
-def __flip_clock_combinations(list_3d):
+def __flip_clock_combinations(list_3d, all_course_classes):
     all_combinations = []
     clock = FlipClock(list_3d)
 
@@ -68,6 +71,22 @@ def __flip_clock_combinations(list_3d):
                 # TODO !!!!!!!!!! ADD is_valid CONTROL !!!!!!!!!!
         clock.shift()  # shift late, clock starts a 0 which is possibly valid
     return all_combinations
+
+
+def __find_from_crn(crn, all_course_classes):
+    """
+    :param crn:
+    INT crn code
+    :param all_course_classes:
+    List of all classes in 3d organized format
+    :return:
+    Return a class object with matching crn, or None if no match was found
+    """
+    for course_sublist in all_course_classes:
+        for class_object in course_sublist:
+            if class_object.crn == crn:
+                return class_object
+    return None
 
 
 def __crn_clean_up(main_schedules_list):

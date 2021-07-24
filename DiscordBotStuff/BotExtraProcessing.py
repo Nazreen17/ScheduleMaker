17f6e5e1a8eb3f30836b95b548b85bc1a3e04cba
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-24 18:16:23 -0400

    Corrected get_clean_courses_list() in BotExtraProcessing.py and also fixed logic to continue testing bot.

--- DiscordBotStuff/BotExtraProcessing.py
+++ DiscordBotStuff/BotExtraProcessing.py
@@ -6,17 +6,28 @@ from ClassStructure.CourseClassStructure import ACourse
 def get_clean_courses_list(course_bot_input):
     """
     :param course_bot_input:
-    A List/Tuple of Course inputs or a single Course input (Math1010u, MATH 1010 U, etc) all auto corrected later
+    STR Course inputs or a single Course input (Math1010U MATH 1010 U, etc) all auto corrected here
     :return:
     A list of Course objects
     """
     all_courses_list = []
 
-    if not isinstance(course_bot_input, list) and not isinstance(course_bot_input, tuple):
-        course_bot_input = [course_bot_input]
+    course_bot_input = course_bot_input.lower().replace(" ", "")  # Lower case and remove spaces
+    course_bot_input = course_bot_input.translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\\|`~-=_+"})
+    # Remove all special characters
 
-    for possible_course in course_bot_input:
-        new_course_obj = __get_course_obj(possible_course.lower().replace(" ", ""))
+    course_str_list = []
+    i = 1
+    while len(course_bot_input) > 1:
+        if course_bot_input[i] == "u" and course_bot_input[i - 1].isdigit():  # Isolate the end of a uid to split by
+            course_str_list.append(course_bot_input[:i + 1])  # Find and save course substring into list (+1 for 'u')
+            course_bot_input = course_bot_input[i + 1:]  # Again, +1 for the 'u'
+            i = 1  # Reset the iterator (since we removed the front end course, gotta start from beginning again)
+        else:
+            i += 1  # No uid ending 'u' found, increase the character shift iterator by 1 to check next character
+
+    for possible_course in course_str_list:
+        new_course_obj = __get_course_obj(possible_course)
 
         if isinstance(new_course_obj, ACourse) and new_course_obj.is_format_valid() is True:
             all_courses_list.append(new_course_obj)
--- DiscordBotStuff/bot.py
+++ DiscordBotStuff/bot.py
@@ -28,7 +28,10 @@ async def ping(ctx):
 async def make_schedule(ctx, *, course_inputs):
     all_courses_list = get_clean_courses_list(course_inputs)
     max_schedules = generate(all_courses_list)
-    ctx.send(f"First: {max_schedules[0]}")
+
+    await ctx.send(f"Generated {len(max_schedules)} schedules")
+    if len(max_schedules) > 0:
+        await ctx.send(f"Index 0: {max_schedules[0]}")  # TODO testing use case only
 
 
 client.run(CLIENT_TOKEN)

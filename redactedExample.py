71ef06666e4b5c4af560f432af51ec838ee26f29
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-25 15:37:21 -0400

    Added redactedExample.py

---
+++ redactedExample.py
@@ -0,0 +1,9 @@
+# MySQL Server login credentials
+SQL_HOST = "123.123.123.123"
+SQL_USERNAME = "daniel"
+SQL_PASSWORD = "SomePassword"
+SQL_DB = "MySQL DB Name"
+
+# Discord Dev Application Bot Client Token
+# https://discord.com/developers/applications
+CLIENT_TOKEN = "TokenHere"

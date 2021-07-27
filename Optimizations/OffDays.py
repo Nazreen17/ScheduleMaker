bb3d290baff2dd0889a9950b2c3ad9a35668baeb
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-27 15:44:20 -0400

    Did a little cleaning up in OffDays.py

--- Optimizations/OffDays.py
+++ Optimizations/OffDays.py
@@ -9,23 +9,7 @@ class OffDay(DualShiftOptimizerStructure):
         self._name = "Days Off"
         self._description = "Get the schedules that have specific days with no classes (or closest possible to)"
         self._max_schedules = schedule_list
-        if isinstance(day_off, str):
-            weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-            if day_off.lower() in weekdays:
-                self._day_off = weekdays.index(day_off.lower())
-            elif len(day_off) == 1:  # if the day int was passed as an int accidentally
-                try:
-                    day_off = int(day_off)
-                    if 0 <= day_off <= 6:
-                        self._day_off = day_off
-                except:
-                    raise ValueError
-            else:
-                raise ValueError
-        elif isinstance(day_off, int) and 0 <= day_off <= 6:
-            self._day_off = day_off
-        else:
-            raise ValueError
+        self._day_off_int = self.__get_day_off_int(day_off)
         self._ties = 0
         self._optimal = self.optimize()
         self._result = "Total ties: " + str(self._ties)
@@ -36,6 +20,30 @@ class OffDay(DualShiftOptimizerStructure):
         """
         super().__init__(name=self._name, description=self._description, max_schedule_list=self._max_schedules,
                          result=self._result, optimal=self._optimal)
+
+    @staticmethod
+    def __get_day_off_int(day_off):
+        if isinstance(day_off, str):
+            day_off = day_off.lower().replace(" ", "")
+            weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+
+            for weekday_partial in weekdays:
+                if weekday_partial in day_off:  # Search the input str to see if the partial weekday name is within
+                    return weekdays.index(weekday_partial)
+                elif len(day_off) == 1 and day_off.isdigit():  # if the day int was passed as an int formatted in str
+                    if 0 <= int(day_off) <= 6:
+                        return int(day_off)
+                    else:
+                        raise ValueError
+                else:
+                    raise ValueError
+
+        elif isinstance(day_off, int) and 0 <= day_off <= 6:
+            # This is mainly for testing cases, since bot will use str inputs mainly
+            return day_off
+
+        else:
+            raise ValueError
 
     def optimize(self):
         best = TermSchedule(self._max_schedules[0])  # initialized first element as the best case
@@ -69,7 +77,7 @@ class OffDay(DualShiftOptimizerStructure):
         day_off = []
         for class_obj in schedule_obj.classes:
             for meet_time_tuple in class_obj.class_time:
-                if meet_time_tuple[0].weekday() == self._day_off:
+                if meet_time_tuple[0].weekday() == self._day_off_int:
                     day_off.append(meet_time_tuple)
         return day_off  # day off will have the meet time inner tuples
 

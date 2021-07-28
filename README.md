3e12db2e5c97f1ecbf5de37f929a3f5f97ea7bff
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-28 15:58:05 -0400

    Removed CORE and updated README.md and setup.py

--- README.md
+++ README.md
@@ -1,26 +1,7 @@
 # ScheduleMaker
-Daniel Jeon
 
-## General About
+**Server-End program**
 
-**Current Python Version: 3.7.9**
-
-## Packages (& Versions) Used
-
-**External Libraries**
-- Pillow (8.3.1)
-  - .png schedule creation
-- selenium (3.141.0)
-  - web scraper
-  - used with ChromeDriver
-- SQLAlchemy (1.4.20)
-  - class object mapping to SQL database
-- mysql-connector-python (8.0.25)
-
-**General Libraries**
-- time
-  - prevent selenium from borking
-- json
-  - general data persistence
-- datetime
-  - time stuff
+- Discord bot implementation
+- MaxSchedule Generation
+- MaxSchedule Optimization
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_namespace_packages
 #     long_description = readme_handle.read()
 
 setup(
-    name="pain-maker",  # Define the library name, this is what is used along with `pip install`.
+    name="ScheduleMaker",  # Define the library name, this is what is used along with `pip install`.
 
     author="Daniel Jeon",
     # author_email="",
@@ -23,23 +23,22 @@ setup(
     # url="",  # GitHub repo link
 
     # Here's a small description of the library. This appears when the library is searched on https://pypi.org/search.
-    description="A python program used to make the Pain Uni Schedules for each term/semester at OTU/UOIT",
-    # long_description=long_description,  # using the README file, variable up above.
+    description="Server-End program",
 
-    long_description_content_type="text/markdown",  # This will specify that the long description is MARKDOWN.
+    # long_description=long_description,  # using the README file, variable up above.
+    # long_description_content_type="text/markdown",  # This will specify that the long description is MARKDOWN.
 
     # Dependencies the library needs in order to run.
     install_requires=[
         "Pillow",
         "discord",
-        "SQLAlchemy",
         "mysql-connector-python"
     ],
 
     # Here are the packages I want build.
     packages=find_namespace_packages(
-        where=["ClassStructure", "JSONCourses", "JSONCourses.*", "JSONMaxTemplates", "Optimizations", "PNGMaker",
-               "PNGMaker.*", "Scraper", "DB"]
+        where=["COREClassStructure", "COREDB", "DiscordBotStuff", "DiscordBotStuff.PNGMaker",
+               "DiscordBotStuff.PNGMaker.*", "MaxSchedule", "Optimizations"]
     ),
 
     # This program requires some package data, such as font files, so include them
@@ -47,24 +46,4 @@ setup(
 
     # mysql connector for python requires python 3.7 if I remember correctly
     python_requires=">=3.7",
-
-    # Characteristics about the package. (https://pypi.org/classifiers/)
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Financial and Insurance Industry",
-
-        "Natural Language :: English",
-
-        "Operating System :: OS Independent",
-
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-
-        "Topic :: Database",
-        "Topic :: Education"
-    ]
 )

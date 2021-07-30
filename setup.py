1edefe5b0775703132b17d1e70c6b82498d96e8a
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-30 14:45:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     #   - MAINTENANCE VERSION 0
     version="0.1.0",
 
-    # url="",  # GitHub repo link
+    url="https://github.com/iDanielJ/ScheduleMaker",  # GitHub repo link
 
     # Here's a small description of the library. This appears when the library is searched on https://pypi.org/search.
     description="Server-End program",

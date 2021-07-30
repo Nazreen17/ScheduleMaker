933537d0253dde9d49b35c2d4d05e4cc51c4ae33
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-07-30 18:24:15 -0400

    Added owner command outline and (commented) commands for max schedule & optimization

--- bot.py
+++ bot.py
@@ -3,17 +3,15 @@
 from discord.ext import commands
 
 from redacted import CLIENT_TOKEN
-from DiscordBotStuff.BotConstants import PREFIX
+from DiscordBotStuff.BotConstants import PREFIX, DEV_IDS
 from DiscordBotStuff.BotExtraProcessing import get_clean_courses_list
 from MaxSchedule.MaxScheduleGeneration import generate
-from DiscordBotStuff.PNGMaker.Pillow import get_discord_file_png_schedule
-from COREDB.ClassPull import pull_class_via_redacted
-from COREClassStructure.TermScheduleStructure import TermSchedule
+from COREDB.MaxTemplatePrivateUpdate import update_private_max_template
 
 # CLIENT_TOKEN = STR Discord dev bot token
 
 
-client = commands.Bot(command_prefix=PREFIX)
+client = commands.Bot(command_prefix=PREFIX, owner_ids=DEV_IDS)
 
 
 @client.event
@@ -21,31 +19,52 @@ async def on_ready():
     print(f"Logged in: {client.user.name} {client.user.id}")
 
 
+@client.command(aliases=["shutdown"])
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.bot.logout()
+
+
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Kinda useless stuff, but ok... {round(client.latency * 1000)} ms")
 
 
+"""
+@client.command(aliases=["wpublic", "writepublic"])
+@commands.is_owner()
+async def generate_public_max_template(ctx):
+    await ctx.bot.logout()
+"""
+
+"""
+@client.command(aliases=["templates", "public"])
+async def view_public_templates(ctx, public_template_id=None):
+    pass
+"""
+
+
 @client.command(aliases=["max"])
-async def make_max_schedule(ctx, *, course_inputs):
+async def generate_private_max_template(ctx, *, course_inputs):
     all_courses_list = get_clean_courses_list(course_inputs)
     max_schedules = generate(all_courses_list)
 
-    await ctx.send(f"Generated {len(max_schedules)} schedules")
+    await ctx.reply(f"Generated {len(max_schedules)} schedules", mention_author=False)
     if len(max_schedules) > 0:
-        await ctx.send(f"Index 0: {max_schedules[0]}")  # TODO testing use case only
+        update_private_max_template(max_schedule=max_schedules, discord_user_id=ctx.message.author.id)
 
-    class_objects_list = []
-    for crn in max_schedules[0]:
-        for course in all_courses_list:
-            class_objects_list += pull_class_via_redacted(fac=course.fac, uid=course.uid, crn=crn)
 
-    temp = TermSchedule()
-    temp.add_class(class_objects_list)
+"""
+@client.command(aliases=["vprivate", "viewprivate"])
+@commands.is_owner()
+async def view_private_templates(ctx):
+    pass
+"""
 
-    file = get_discord_file_png_schedule(temp)
-
-    await ctx.send(file=file)
-
+"""
+@client.command(aliases=["optimize"])
+async def optimize(ctx, template_id=None):
+    template_id = ctx.message.author.id if template_id is None else template_id
+"""
 
 client.run(CLIENT_TOKEN)

6eaae170b368b781ffed5ee5e6bbe89281db56d5
Daniel J <48095779+danielljeon@users.noreply.github.com>
2021-08-01 17:23:14 -0400

    Added a forgotten "= if else" in commit 5fe69ec56d01c626bdc42b09c654ccdc3143eb41

--- FullProcess/Processing.py
+++ FullProcess/Processing.py
@@ -58,7 +58,7 @@ def generate_png_and_txt(single_term_schedule, result_txt_header_str=None):
     # Generate schedule.png
     draw_png_schedule(single_term_schedule)
 
-    header = f"{result_txt_header_str}\n------------------------------\n"
+    header = f"{result_txt_header_str}\n------------------------------\n" if result_txt_header_str is not None else ""
 
     # Generate results.txt
     with open("DiscordBotStuff/result.txt", "w") as file:

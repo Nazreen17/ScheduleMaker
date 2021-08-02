e8751df3c4b07cbd5a06d3b9d7748ba7a4eca65d
Daniel J <48095779+iDanielJ@users.noreply.github.com>
2021-08-02 01:50:38 -0400

    Added requesting course logic for DB control Also added new main constant to limit number of course requests saved to the DB. Used some basic level of documentation in my code for once :P

--- constants.py
+++ constants.py
@@ -6,5 +6,7 @@ from Optimizations.InPersonNot import Online
 
 MAX_SCHEDULE_COMBINATIONS = 100000
 
+MAX_COURSE_UPDATE_REQUESTS = 25
+
 ENABLED_OPTIMIZER_OBJECT_LIST = [EarlyEnd(), DayOff(), InPerson(), Online()]
 # ALSO UPDATE: FullProcess/CallOptimizers.py -> __initialize_optimizer()

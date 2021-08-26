c20d9bbe3acf871202dd5815b8c7c5e8958b1353
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2021-08-26 19:24:46 -0400

    Improved Documentation. - Added OptimizerExample.py. - Better comments and stuff.

--- Optimizations/DualShiftOptimizerStructure.py
+++ Optimizations/DualShiftOptimizerStructure.py
@@ -6,9 +6,10 @@ from COREClassStructure.TermScheduleStructure import TermSchedule
 class DualShiftOptimizer(ABC):
     def __init__(self, schedule_list):
         self._max_schedules = schedule_list
-        self._ties = []
+        self._ties = []  # list -> List of TermSchedule objects tied for the best case of the requested Optimizer
         # _ties attribute init must be called before _optimal init because _ties is required in optimize().
-        # The optimize() function is used in setting the _optimal value
+        # The optimize() function is used in calculating and setting the _optimal value
+
         if self._max_schedules is not None and self._ties == []:
             self.__optimize()
 
@@ -28,24 +29,51 @@ class DualShiftOptimizer(ABC):
         pass
 
     def __optimize(self):
+        """
+        void -> Calculate the ties attribute/property and set the self._ties attribute
+        """
         if len(self._max_schedules) == 0:
             raise ValueError("len(max_schedules) = 0")
 
         best = TermSchedule(self._max_schedules[0])  # Initialized first element as the best case
         self._ties.append(best)  # Initialize best into the ties
+
         for crn_list_i in range(1, len(self._max_schedules)):  # cycle all in schedule list
             current = TermSchedule(self._max_schedules[crn_list_i])
             best = self.compare_for_best(best, current)
 
     @abstractmethod
     def compare_for_best(self, best, current):
+        """
+        Check __OptimizerExample() from OptimizerExample.py for an example
+        Abstract method is pushed to a a subclass
+
+        compare_for_best() is called by the super class's __optimize() method.
+        __optimize() compares the current and best case TermSchedule
+        :param best:
+        TermSchedule -> The best TermSchedule so far
+        :param current:
+        TermSchedule -> The current TermSchedule to compare to the best
+        :return:
+        TermSchedule -> Return the best case TermSchedule object
+        """
         pass
 
     @property
     def ties(self):
+        """
+        property getter
+        :return:
+        attribute -> self._ties
+        """
         return self._ties
 
     def ties_add_from_term_schedule(self, term_schedule):
+        """
+        void -> Should/Is called during compare_for_best() to add a new TermSchedule in the case of a tie
+        :param term_schedule:
+        TermSchedule -> Object to add to the attribute list
+        """
         if isinstance(term_schedule, TermSchedule):
             self._ties.append(term_schedule)
         else:
@@ -53,6 +81,11 @@ class DualShiftOptimizer(ABC):
 
     @ties.setter
     def ties(self, ties):
+        """
+        property setter (Setter mostly used for testing purposes)
+        :param ties:
+        TermSchedule, list, tuple -> Set as the new ties
+        """
         if isinstance(ties, TermSchedule):
             self._ties = [ties]
         elif isinstance(ties, list):
---
+++ Optimizations/OptimizerExample.py
@@ -0,0 +1,59 @@
+from Optimizations.DualShiftOptimizerStructure import DualShiftOptimizer
+
+
+class __OptimizerExample(DualShiftOptimizer):
+    def __init__(self, schedule_list=None):
+        super().__init__(schedule_list=schedule_list)
+
+    @property
+    def name(self):
+        return "OptimizerExampleName"
+
+    @property
+    def description(self):
+        return "Optimizer Description (What the optimizer does)"
+
+    @property
+    def result(self):
+        # Result string text
+        result = "" if self.ties == [] else f"Total ties count: {len(self.ties)}"
+        return result
+
+    @staticmethod
+    def __calculate_score(schedule_obj):
+        """
+        used in optimize
+        :param schedule_obj:
+        :return:
+        """
+        example_score = 0
+
+        for class_obj in schedule_obj.classes:
+            pass
+
+        return example_score
+
+    def compare_for_best(self, best, current):
+        """
+        compare_for_best() is called by the super class's __optimize() method.
+        __optimize() compares the current and best case TermSchedule
+        :param best:
+        TermSchedule -> The best TermSchedule so far
+        :param current:
+        TermSchedule -> The current TermSchedule to compare to the best
+        :return:
+        TermSchedule -> Return the best case TermSchedule object
+        """
+        best_case_score = self.__calculate_score(best)
+        current_case_score = self.__calculate_score(current)
+
+        if best_case_score == current_case_score:  # Tie of scores
+            self.ties_add_from_term_schedule(current)  # Add the current TermSchedule as a tied case
+            return best  # Default tie -> return previous best
+
+        elif best_case_score > current_case_score:  # Best case has a better score
+            return best
+
+        else:  # Current case has a better score
+            self.ties = current  # Reset ties
+            return current

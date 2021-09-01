611ca65e8531adf55530408392ff625fc7aa3361
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2021-08-31 23:49:21 -0400

    Improved CallResultTextGenerate.py

--- FullProcess/CallResultTextGenerate.py
+++ FullProcess/CallResultTextGenerate.py
@@ -29,24 +29,25 @@ def warning_message_text(term_schedule, search_crn_codes=None):
 
     # Check for possible bad crn codes if search_crn_codes is not None
     if search_crn_codes is not None and len(term_schedule) != len(search_crn_codes):
-        found_crn_codes_str = [class_object.crn for class_object in term_schedule.classes]
+        found_crn_codes_str = [str(class_object.crn) for class_object in term_schedule.classes]
+        crn_codes_not_found = set(search_crn_codes) - set(found_crn_codes_str)
+
         # Combine all found crn codes
-        warning_message.append(f"WARNING: Could not find {', '.join(set(search_crn_codes) - set(found_crn_codes_str))}.")
+        warning_message.append(f"WARNING -> Could not find CRN codes: {', '.join(crn_codes_not_found)}")
 
     # Check for possible time conflict
     if not term_schedule.is_time_valid():
-        warning_message.append("WARNING: Time conflict detected.")
+        warning_message.append("WARNING -> Time conflict detected")
 
     # Check for link satisfaction
     is_links_satisfied, offender = term_schedule.all_links_satisfied_with_a_class_offender()
     if not is_links_satisfied:
-        warning_message.append(f"WARNING: Class links are unsatisfied! First offender: "
-                               f"{offender.fac}{offender.uid} ({offender.crn})")
+        warning_message.append(f"WARNING -> Class links are unsatisfied! First offender: {offender.fac}{offender.uid}")
 
     # Check for open seats
     is_open_seats, offender = term_schedule.all_open_seats_with_a_class_offender()
     if not is_open_seats:
-        warning_message.append(f"WARNING: Class(es) may have no seats left! First offender: "
+        warning_message.append(f"WARNING -> Class(es) may have no seats left! First offender: "
                                f"{offender.fac}{offender.uid} ({offender.crn})\n")
 
     # Final format

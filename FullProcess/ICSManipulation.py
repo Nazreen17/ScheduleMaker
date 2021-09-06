04eb3e058d0e5e5354e0551a0b3590e4f72c6330
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2021-09-06 19:40:02 -0400

    Added multi day weekly ics event creation.

--- FullProcess/ICSManipulation.py
+++ FullProcess/ICSManipulation.py
@@ -23,7 +23,7 @@ def create_calendar_from_term_schedule(term_schedule, user_id=None):
     """
     calendar = Calendar()
 
-    events = __build_event_list_from_a_class(term_schedule.classes.copy())
+    events = __build_event_list_from_a_class(term_schedule.classes)
     events += __build_exclusion_event_list()
 
     for event in events:
@@ -51,29 +51,57 @@ def __build_event_list_from_a_class(a_class_list):
 
     for a_class in a_class_list:
         if len(a_class.class_time) > 0:  # Ensure at least 1 class meet time tuple
-            for meet_time_tuple in a_class.class_time:
-
-                if a_class.is_biweekly_specific():  # biweekly, disable recurrence
-                    events_list.append(__build_recurring_events(a_class=a_class, meet_time_tuple=meet_time_tuple,
-                                                                biweekly_specific=True))
-
-                else:  # Not biweekly, enable recurrence only should be required on non biweekly specific class
-                    events_list.append(__build_recurring_events(a_class=a_class, meet_time_tuple=meet_time_tuple,
-                                                                biweekly_specific=False))
+            events_list += __build_recurring_events(a_class)
 
     return events_list
 
 
-def __build_recurring_events(a_class, meet_time_tuple, biweekly_specific=True):
+def __build_recurring_events(a_class):
     """
     :param a_class:
     AClass -> to get basic information from
-    :param meet_time_tuple:
-    tuple -> datetime.datetime standard 2 element meet time tuple, used to set actual event times
-    :param biweekly_specific:
-    bool -> True for biweekly specific, else False, Default True (True means no recurrence)
     :return:
-    icalendar.Event -> translated AClass
+    list -> of icalendar.Event(s) of translated AClass
+    """
+    events_list = []
+
+    if not a_class.is_biweekly_specific() and not a_class.not_biweekly_different_times():  # Not biweekly, and all same
+        # class_times. Create 1 event with weekly recurrence
+
+        single_event = __initialize_event(a_class)
+
+        # Datetime start and end setting
+        first_start, first_end = __first_start_and_end_after_sem_start(a_class.class_time.copy())
+        single_event.add("dtstart", first_start)  # Create first event start and end
+        single_event.add("dtend", first_end)
+
+        # Recurrence/Repeating frequency rule setting
+        day_dict = {0: "MO", 1: "TU", 2: "WE", 3: "TH", 4: "FR", 5: "SA",
+                    6: "SU"}  # Heads up: datetime uses 0=Monday VS icalendar uses 0=Sunday
+        days = [day_dict[meet_time[0].weekday()] for meet_time in a_class.class_time]  # List of all days for class
+        single_event.add("rrule", {"FREQ": "WEEKLY", "UNTIL": SEMESTER_END, "BYDAY": days})  # Recurrence rule
+
+        events_list.append(single_event)
+
+    else:  # Is biweekly specific or (is not biweekly class times on different days and times). Create multiple events.
+        for meet_time in a_class.class_time:
+            multi_event = __initialize_event(a_class)
+
+            # Datetime start and end setting
+            multi_event.add("dtstart", meet_time[0])  # Create first event start and end
+            multi_event.add("dtend", meet_time[1])
+
+            events_list.append(multi_event)
+
+    return events_list
+
+
+def __initialize_event(a_class):
+    """
+    :param a_class:
+    AClass -> class object to translate to event
+    :return:
+    icalendar.Event() -> class translated to event
     """
     event = Event()
 
@@ -81,33 +109,53 @@ def __build_recurring_events(a_class, meet_time_tuple, biweekly_specific=True):
     summary = f"{a_class.title} {a_class.class_type} ({a_class.fac}{a_class.uid})"
     event.add("summary", summary)
 
+    # Format str data used more than once
+    campus_str_data = ' | '.join(a_class.campus) if a_class.campus is not None and len(a_class.campus) > 0 else 'N/A'
+    building_str_data = (' | '.join(a_class.building) if a_class.building is not None and len(a_class.building) > 0
+                         else 'N/A')
+    room_str_data = ' | '.join(a_class.room) if a_class.building is not None and len(a_class.room) > 0 else 'N/A'
+
     # Description setting
-    description = (f"Professor: {' | '.join(a_class.prof) if len(a_class.prof) > 0 else 'N/A'}\n"
-                   f"CRN: {a_class.crn}\n"
-                   f"Section: {a_class.section}\n"
-                   f"Campus: {' | '.join(a_class.campus) if len(a_class.campus) > 0 else 'N/A'}\n"
-                   f"Building: {' | '.join(a_class.building) if len(a_class.building) > 0 else 'N/A'}\n"
-                   f"Room: {' | '.join(a_class.room) if len(a_class.room) > 0 else 'N/A'}")
+    description = (
+        f"Professor: {' | '.join(a_class.prof) if a_class.prof is not None and len(a_class.prof) > 0 else 'N/A'}\n"
+        f"CRN: {a_class.crn}\n"
+        f"Section: {a_class.section}\n"
+        f"Campus: {campus_str_data}\n"
+        f"Building: {building_str_data}\n"
+        f"Room: {room_str_data}")
     event.add("description", description)
 
     # Location setting
-    location = (f"Campus: {' | '.join(a_class.campus)} & Building: {' | '.join(a_class.building)} & "
-                f"Room: {' | '.join(a_class.room)}")
+    location = (
+        f"Campus: {campus_str_data} & "
+        f"Building: {building_str_data} & "
+        f"Room: {room_str_data}")
     event.add("location", location)
-
-    # Datetime start and end setting
-    if SEMESTER_START < meet_time_tuple[0] < SEMESTER_END and SEMESTER_START < meet_time_tuple[1] < SEMESTER_END:
-        event.add("dtstart", meet_time_tuple[0])
-        event.add("dtend", meet_time_tuple[1])
-
-    # Recurrence/Repeating frequency rule setting
-    if not biweekly_specific:
-        event.add("rrule", {"FREQ": "WEEKLY", "UNTIL": SEMESTER_END})
 
     # Datetime stamp setting
     event.add("dtstamp", datetime.now())
 
     return event
+
+
+def __first_start_and_end_after_sem_start(class_times):
+    """
+    :param class_times:
+    AClass -> Get first start and end of a meet time
+    :return:
+    datetime.datetime, datetime.datetime -> of start and end datetime.datetime of the first meet time after the
+    SEMESTER_START datetime
+    """
+    best = None
+
+    for meet_time_tuple in class_times:
+        if SEMESTER_START < meet_time_tuple[0] and (best is None or meet_time_tuple < best):  # (Just use start time
+            # for comparisons)
+            # meet_time_start is greater/newer than the SEMESTER_START, then... best is not set or the new meet_time is
+            # better (More closer be age to the SEMESTER_START)
+            best = meet_time_tuple
+
+    return best  # Return best meet_time_tuple (best_start_datetime, best_end_datetime)
 
 
 def __build_exclusion_event_list():
